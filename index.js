583f4aea8a4d9b2320ca72007e8100f05c5412a1
Lakhiwe <siyasangalakhiwe6@gmail.com>
2017-10-12 14:43:26 +0200

    push to heroku

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const exphbs = require("express-handlebars");
 const form = require('body-parser');
-const flash = require('express-flash');
-const session = require('express-session');
+// const flash = require('express-flash');
+// const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/test";
 const Models = require("./model");
@@ -18,13 +18,13 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(session({
-    secret: 'keyboard cat',
-    cookie: {
-        maxAge: 60000 * 30
-    }
-}));
-app.use(flash());
+// app.use(session({
+//     secret: 'keyboard cat',
+//     cookie: {
+//         maxAge: 60000 * 30
+//     }
+// }));
+// app.use(flash());
 
 // setting rendering engine
 app.engine("hbs", exphbs({
